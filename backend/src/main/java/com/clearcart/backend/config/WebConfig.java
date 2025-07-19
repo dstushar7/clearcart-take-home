a4562b0fc21fc6f737f3278fa10652464705b4c9
dstushar7 <tusharsarker97@gmail.com>
2025-07-19 10:08:25 +0600

    feat(dockerize): Docker setup is complete. Whole project running as bundle

--- backend/src/main/java/com/clearcart/backend/config/WebConfig.java
+++ backend/src/main/java/com/clearcart/backend/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.clearcart.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(final CorsRegistry registry){
         registry.addMapping("/graphql")
-                .allowedOrigins("http://localhost:5173") // Use the specific origin
+                .allowedOrigins(allowedOrigins) // Use the specific origin
                 .allowedMethods("POST", "GET", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
